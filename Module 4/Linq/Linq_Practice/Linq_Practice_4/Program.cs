b9ea5aedfc52ce577b8764363a89b594f0a5f756
Серкан Дерели <sdereli@edu.hse.ru>
2020-05-28 03:25:55 +0300

    Практика линка. 2 Подход

--- Module 4/Linq/Linq_Practice/Linq_Practice_4/Program.cs
+++ Module 4/Linq/Linq_Practice/Linq_Practice_4/Program.cs
@@ -63,9 +63,42 @@ namespace Linq_Practice_4
             }
         }
 
+        private static void ComplexFiltrationWithMethod()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Фильтруем массив с помощью метода-расширения:");
+
+            List<User> users = new List<User>
+                {
+                new User {Name="Том", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+                new User {Name="Боб", Age=27, Languages = new List<string> {"английский", "французский" }},
+                new User {Name="Джон", Age=29, Languages = new List<string> {"английский", "испанский" }},
+                new User {Name="Элис", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+                };
+
+            Console.WriteLine();
+            Console.WriteLine("Исходный массив: ");
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(user);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Фильтрованный массив: ");
+            IEnumerable<User> spanish = users.SelectMany(u => u.Languages, (user, language) => new { User = user, Language = language }).Where(u => u.Language == "испанский" && u.User.Age > 20)
+                          .Select(u => u.User);
+            foreach (User user in spanish)
+            {
+                Console.WriteLine(user);
+            }
+        }
+
         static void Main(string[] args)
         {
             ComplexFiltration();
+
+            ComplexFiltrationWithMethod();
         }
     }
 }
---
+++ Module 4/Linq/Linq_Practice/Linq_Practice_5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Изучение проекций
+namespace Linq_Practice_5
+{
+
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name : {Name}, Age: {Age}";
+        }
+    }
+
+    
+
+    class Program
+    {
+        private static void SelectNamesWithLinq()
+        {
+            Console.WriteLine("Выбираем свойство объекта с помощью линка:");
+            List<User> users = new List<User>();
+            users.Add(new User { Name = "Sam", Age = 43 });
+            users.Add(new User { Name = "Tom", Age = 33 });
+
+            Console.WriteLine();
+            Console.WriteLine("Исходный массив: ");
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(user);
+            }
+
+            var names = from user in users
+                        select user.Name;
+
+            Console.WriteLine();
+            Console.WriteLine("Только имена пользователей: ");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        private static void CreateAnonymousTypeWIthLinq()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Возвращаем анонимный тип с помощью линка: ");
+            List<User> users = new List<User>();
+            users.Add(new User { Name = "Sam", Age = 43 });
+            users.Add(new User { Name = "Tom", Age = 33 });
+
+            Console.WriteLine();
+            Console.WriteLine("Исходный массив: ");
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(user);
+            }
+
+            var items = from u in users
+                        select new
+                        {
+                            FirstName = u.Name,
+                            DateOfBirth = DateTime.Now.Year - u.Age
+                        };
+
+            Console.WriteLine();
+            Console.WriteLine("Фильтрованный массив: ");
+            foreach (var n in items)
+                Console.WriteLine($"{n.FirstName} - {n.DateOfBirth}");
+        }
+
+        static void Main(string[] args)
+        {
+            SelectNamesWithLinq();
+
+            CreateAnonymousTypeWIthLinq();
+        }
+    }
+}
