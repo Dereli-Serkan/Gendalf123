7ad773bde1cbef46950a4e4862669e97b6ee068f
Серкан Дерели <sdereli@edu.hse.ru>
2020-06-03 22:40:04 +0300

    Потоки. Практика. 1 подход

---
+++ Module 4/Threads/Threads_Practice/Threads_Practice/Thread_Practice_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем новый поток
+            Thread myThread = new Thread(new ThreadStart(Count));
+            myThread.Start(); // запускаем поток
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Главный поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Count()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Второй поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
---
+++ Module 4/Threads/Threads_Practice/Threads_Practice/Threading_Practice_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading_Practice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            counter.x = 4;
+            counter.y = 5;
+
+            Thread myThread = new Thread(new ParameterizedThreadStart(Count));
+            myThread.Start(counter);
+
+            //...................
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("Основной поток: " + i*counter.x * counter.y);
+                Thread.Sleep(400);
+            }
+        }
+
+        public static void Count(object obj)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Counter c = (Counter)obj;
+
+                Console.WriteLine("Второй поток:" + i * c.x * c.y);
+                Thread.Sleep(400);
+            }
+        }
+    }
+
+    public class Counter
+    {
+        public int x;
+        public int y;
+    }
+}
+
